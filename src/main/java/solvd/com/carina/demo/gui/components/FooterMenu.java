e0dcfa5975fb13f2f0851443341e738802ff796d
Ivan Castellanos <icastellanos@solvd.com>
2024-10-23 09:26:50 -0300

    feat: initial commit

---
+++ src/main/java/solvd/com/carina/demo/gui/components/FooterMenu.java
@@ -0,0 +1,42 @@
+package solvd.com.carina.demo.gui.components;
+
+import org.openqa.selenium.SearchContext;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.FindBy;
+
+import com.zebrunner.carina.webdriver.decorator.ExtendedWebElement;
+import com.zebrunner.carina.webdriver.gui.AbstractUIObject;
+import solvd.com.carina.demo.gui.pages.CompareModelsPage;
+import solvd.com.carina.demo.gui.pages.HomePage;
+import solvd.com.carina.demo.gui.pages.NewsPage;
+
+public class FooterMenu extends AbstractUIObject {
+
+    @FindBy(linkText = "Home")
+    private ExtendedWebElement homeLink;
+
+    @FindBy(xpath = "//div[@class='footer-inner']//a[contains(text(),'Compare')]")
+    private ExtendedWebElement compareLink;
+    
+    @FindBy(linkText = "News")
+    private ExtendedWebElement newsLink;
+
+    public FooterMenu(WebDriver driver, SearchContext searchContext) {
+        super(driver, searchContext);
+    }
+
+    public HomePage openHomePage() {
+        homeLink.click();
+        return new HomePage(driver);
+    }
+
+    public CompareModelsPage openComparePage() {
+        compareLink.click();
+        return new CompareModelsPage(driver);
+    }
+    
+    public NewsPage openNewsPage() {
+        newsLink.click();
+        return new NewsPage(driver);
+    }
+}
